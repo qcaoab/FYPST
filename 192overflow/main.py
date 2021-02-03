d2952c746b6a6302100be235789598436d9c6075
qinglancao <qcaoab@gmail.com>
2021-02-03 15:11:02 +0800

    3pm

--- 192overflow/main.py
+++ 192overflow/main.py
@@ -1,41 +1,48 @@
 import os
-from app import app
+from uuid import uuid4
 import urllib.request
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template,send_from_directory
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+location = 'C:/Users/qlcql/Documents/GitHub/FYPST'
+HTML = os.path.join(location, '/template/upload.html')
+app = Flask(__name__)
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 	
 @app.route('/')
 def upload_form():
-	return render_template('../html/files/empty.html')
+	return render_template(HTML)
 
 @app.route('/', methods=['POST'])
 def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
     if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed')
-		return render_template('../html/files/empty.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect(request.url)
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed')
+        return render_template(HTML, filename=filename)
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 @app.route('/display/<filename>')
 def display_image(filename):
-	#print('display_image filename: ' + filename)
-	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+    print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='../pic/uploads/' + filename), code=301)
 
 if __name__ == "__main__":
     app.run()
